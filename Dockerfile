95a5fba7c873f27c6f169f54c0dedda2ab378b29
syedhassaanahmed <hassaan.brix@gmail.com>
2025-03-20 22:33:53 +0000

    Introduce OTel Collector

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM python:3.12-bullseye
 
-# Install uv.
+# Install uv
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 
 COPY main.py pyproject.toml uv.lock /app/
 WORKDIR /app
 RUN uv sync --frozen --no-cache
 
-# Run the application.
-CMD ["./.venv/bin/fastapi", "run", "main.py", "--port", "80", "--host", "0.0.0.0"]
+EXPOSE 8000
+
+# Run the application
+CMD ["./.venv/bin/fastapi", "run", "main.py", "--port", "8000", "--host", "0.0.0.0"]
--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from fastapi import FastAPI
-from opentelemetry import trace
 from opentelemetry import metrics
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+import os
+
+# Set up OpenTelemetry metrics
+endpoint = os.getenv("OTLP_ENDPOINT", "http://localhost:4317")
+reader = PeriodicExportingMetricReader(
+    OTLPMetricExporter(endpoint=endpoint)
+)
+resource = Resource(attributes={
+    SERVICE_NAME: __name__
+})
+meterProvider = MeterProvider(resource=resource, metric_readers=[reader])
+metrics.set_meter_provider(meterProvider)
+meter = metrics.get_meter("item.counter.meter")
+item_counter = meter.create_counter(
+    "item.counter", unit="items", description="Counts the number of items created"
+)
 
 app = FastAPI()
-
 
 @app.get("/")
 def get():
     return {"message": "Hello World"}
 
 @app.post("/items/{quantity}")
-async def create(quantity: int):
+def create(quantity: int):
+    item_counter.add(quantity, attributes={"item.type": "item_type_1"})
     return {"message": f"{quantity} items created"}
